9ce19adde0ead45fd3c526af257dd86ea3671dce
Akhilesh <akhileshay805@gmail.com>
2026-03-07 17:08:28 +0530

    Web search function(get_web_urls) created

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ import asyncio
 import streamlit as st
 from duckduckgo_search import DDGS
 
+def get_web_urls(search_term: str, numresults: int =10,)-> list[str]:
+    try:
+        discard_url=["youtube.com","bitanica.com","vimeo.com"]
+        for url in discard_url:
+            search_term += f"-site{url}"
+        
+        st.write(search_term)
+
+        results=DDGS().text(search_term,max_results=numresults)
+        st.write(results)
+        return results
+    
+    except Exception as e:
+        error_msg=("Failed to fetch the result from the web ",str(e))
+        print(error_msg)
+        st.write(error_msg)
+        st.stop()
 async def run():
     #Title of the page
     st.set_page_config(page_title="LLM with Web Search")
@@ -19,6 +36,12 @@ async def run():
     go=st.button(
         "GO",
     )
+    if prompt and go:
+        if is_web_search:
+            web_urls=get_web_urls(search_term=prompt)
+            if not web_urls:
+                st.write("No result found")
+                st.stop()
 
 if __name__=="__main__":
     asyncio.run(run())
